dd7f744ad321484e8881087c80089e02466a6559
Hlynka Oleh <hlynkawork@gmail.com>
2024-05-15 04:37:47 +0300

    test push

--- packages/contracts/auth/validate-token/index.ts
+++ packages/contracts/auth/validate-token/index.ts
@@ -3,6 +3,13 @@ import {
   ApiGatewayContract,
   HttpStatusCodes,
 } from '@swarmion/serverless-contracts';
+import { JSONSchema } from 'json-schema-to-ts';
+
+const headersSchema = {
+  type: 'object',
+  properties: { authorization: { type: 'string' } },
+  required: ['authorization'],
+} as const satisfies JSONSchema;
 
 export const validateTokenContract = new ApiGatewayContract({
   id: 'validateToken',
@@ -11,6 +18,7 @@ export const validateTokenContract = new ApiGatewayContract({
   integrationType: 'restApi',
   authorizerType: 'cognito',
   requestContextSchema,
+  headersSchema,
   outputSchemas: {
     [HttpStatusCodes.OK]: {
       type: 'object',
