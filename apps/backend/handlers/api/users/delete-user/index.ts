81b099db07f2069941c1d5dd84a55901280ba55b
Hlynka Oleh <hlynkawork@gmail.com>
2024-05-22 14:21:28 +0300

    fix

--- apps/backend/handlers/api/users/delete-user/index.ts
+++ apps/backend/handlers/api/users/delete-user/index.ts
@@ -11,21 +11,37 @@ import {
 } from '@/middlewares/database-connection-middleware';
 import { USERS_COLLECTION } from '@/common/constants/database-constants';
 import doNotWaitForEmptyEventLoop from '@middy/do-not-wait-for-empty-event-loop';
+import {
+  AdminDeleteUserCommand,
+  CognitoIdentityProviderClient,
+  DeleteUserCommand,
+} from '@aws-sdk/client-cognito-identity-provider';
 
-const main = getHandler(deleteUserContract, { ajv })(async (event, context) => {
-  const { db } = context as DbConnectionContext;
-  const { sub: cognitoId } = event.requestContext.authorizer.jwt.claims;
+const client = new CognitoIdentityProviderClient({});
 
-  const deletedItem = await db
-    .collection(USERS_COLLECTION)
-    .findOneAndDelete({ cognitoId });
+const main = getHandler(deleteUserContract, { ajv, validateOutput: false })(
+  async (event, context) => {
+    const { db } = context as DbConnectionContext;
+    const { sub: cognitoId } = event.requestContext.authorizer.jwt.claims;
 
-  if (!deletedItem) {
-    throw new Error('User not found', { cause: HttpStatusCodes.NOT_FOUND });
-  }
+    const deletedItem = await db
+      .collection<UserType>(USERS_COLLECTION)
+      .findOneAndDelete({ cognitoId });
 
-  return httpResponse(deletedItem as unknown as UserType);
-});
+    if (!deletedItem) {
+      throw new Error('User not found', { cause: HttpStatusCodes.NOT_FOUND });
+    }
+
+    await client.send(
+      new AdminDeleteUserCommand({
+        UserPoolId: process.env.USER_POOL_ID!,
+        Username: deletedItem.email,
+      }),
+    );
+
+    return httpResponse(deletedItem as unknown as UserType);
+  },
+);
 
 export const handler = middy(main)
   .use(doNotWaitForEmptyEventLoop())
